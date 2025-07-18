19ec44ea1b45b2a6bbea28b9d603ced2cb8e54b4
foXaCe <foxace66@gmail.com>
2025-07-18 09:12:04 +0200

    Update __init__.py

--- custom_components/start_time/__init__.py
+++ custom_components/start_time/__init__.py
@@ -1,10 +1,11 @@
 from homeassistant.config_entries import ConfigEntry, SOURCE_IMPORT
 from homeassistant.core import HomeAssistant
+from homeassistant.const import Platform
 
 from .sensor import DOMAIN, StartTime
 
-
 async def async_setup(hass: HomeAssistant, hass_config: dict):
+    """Set up the Start Time component."""
     hass.data[DOMAIN] = StartTime()
 
     if DOMAIN in hass_config and not hass.config_entries.async_entries(DOMAIN):
@@ -13,9 +14,11 @@ async def async_setup(hass: HomeAssistant, hass_config: dict):
         ))
     return True
 
-
-async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry):
-    hass.async_create_task(hass.config_entries.async_forward_entry_setup(
-        config_entry, 'sensor'
-    ))
+async def async_setup_entry(hass: HomeAssistant, config_entry: ConfigEntry) -> bool:
+    """Set up Start Time from a config entry."""
+    await hass.config_entries.async_forward_entry_setups(config_entry, [Platform.SENSOR])
     return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    return await hass.config_entries.async_unload_platforms(entry, [Platform.SENSOR])
